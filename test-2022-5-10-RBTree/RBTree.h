395e29cd34e2b330952ec18aed695ecba1c4f01f
ls02 <2877455773@qq.com>
2022-05-10 18:10:36 +0800

    "RBTree"

--- test-2022-5-10-RBTree/RBTree.h
+++ test-2022-5-10-RBTree/RBTree.h
@@ -2,6 +2,59 @@
 #ifndef __RBTREE_H__
 #define __RBTREE_H__
 #include <iostream>
+#include <utility>
+
+namespace ls
+{
+	template<class K, class V>
+	struct RBTreeNode
+	{
+		enum color
+		{
+			RED,
+			BLACK
+		};
+		RBTreeNode<K, V>* _left;
+		RBTreeNode<K, V>* _right;
+		RBTreeNode<K, V>* _parent;
+
+		color _col;
+		std::pair<K, V> _kv;
+
+		RBTreeNode(std::pair<K, V> kv)
+			: _left(nullptr)
+			,_right(nullptr)
+			,_parent(nullptr)
+			,_kv(kv)
+			,_col(RED)
+		{}
+	};
+
+	template<class K, class V>
+	class RBTree
+	{
+	private:
+		typedef RBTreeNode<K, V> Node;
+		Node* _root;
+
+	public:
+		RBTree()
+			:_root(nullptr)
+		{}
+
+		std::pair<Node*, bool> Insert(std::pair<K, V> kv)
+		{
+			if (nullptr == _root)
+			{
+				_root = new Node(kv);
+
+				return std::make_pair(_root, true);
+			}
+
+
+		}
+	};
+}
 
 
 #endif
