57fcd3c5e273e68edd53d2adb698a62a6d770bd6
ls02 <2877455773@qq.com>
2022-05-26 22:34:03 +0800

    practice code

--- test-2022-5-26-1/test.cpp
+++ test-2022-5-26-1/test.cpp
@@ -1,6 +1,69 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
 
-int main(void)
+
+////饿汉模式
+//class Singleton
+//{
+//private:
+//	Singleton()
+//		:_a(0)
+//	{};
+//
+//	Singleton(const Singleton&) = delete;
+//	Singleton& operator=(const Singleton&) = delete;
+//
+//	size_t _a;
+//	static Singleton* _inst;
+//
+//public:
+//	static Singleton* GetInstance(void)
+//	{
+//		return _inst;
+//	}
+//};
+//
+//Singleton* Singleton::_inst = new Singleton;
+
+//懒汉模式
+class Singleton
+{
+private:
+	Singleton()
+		:_a(0)
+	{};
+
+	Singleton(const Singleton&) = delete;
+	Singleton& operator=(const Singleton&) = delete;
+
+	size_t _a;
+	static Singleton* _inst;
+	static std::mutex _mut;
+
+public:
+	static Singleton* GetInstance(void)
+	{
+		if (nullptr == _inst)
+		{
+			_mut.lock();
+
+			if (nullptr == _inst)
+			{
+				_inst = new Singleton;
+			}
+
+			_mut.unlock();
+		}
+		return _inst;
+	}
+};
+
+std::mutex Singleton::_mut;
+
+Singleton* Singleton::_inst = nullptr;
+
+void Test1(void)
 {
 	int a = 5;
 	int b = 2;
@@ -31,6 +94,22 @@ int main(void)
 	f3();
 
 	std::cout << a << " " << b << std::endl;
+}
+
+void Test2(void)
+{
+	Singleton* a;
+	Singleton* b;
+	Singleton* c;
+
+	std::cout << a->GetInstance() << std::endl;
+	std::cout << b->GetInstance() << std::endl;
+	std::cout << c->GetInstance() << std::endl;
+}
+
+int main(void)
+{
+	Test2();
 
 	return 0;
 }
