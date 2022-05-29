10bf4e62a8ef2369c9784e046189317ee24ed7e7
ls02 <2877455773@qq.com>
2022-05-29 14:40:55 +0800

    practice code

--- test-2022-5-27-1/test.cpp
+++ test-2022-5-27-1/test.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 
-int main(void)
+int gcd(int a, int b)
 {
-	std::cout << "hello" << std::endl;
-
-	return 0;
+    if (a % b == 0) return b;
+    else return gcd(b, a % b);
 }
+int x, y;
+int main() {
+
+    std::cin >> x >> y;
+    std::cout << gcd(x, y);
+
+    return 0;
+}
+
+//int main(void)
+//{
+//	//std::cout << "hello" << std::endl;
+//	int flag = 1;
+//	double ret = 0.0;
+//
+//	for (int i = 1; i <= 100; i++)
+//	{
+//		ret += (double)flag / i;
+//		flag *= -1;
+//	}
+//
+//
+//	std::cout << ret;
+//
+//	return 0;
+//}
